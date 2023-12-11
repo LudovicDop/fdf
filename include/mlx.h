89bb6a990f2adf897dcecee18450f103b616d280
Ludovic Doppler <ldoppler@k2r2p2.42mulhouse.fr>
2023-12-11 16:24:08 +0100

    add MLX

--- include/mlx.h
+++ include/mlx.h
@@ -5,5 +5,11 @@
 # include "MLX42/MLX42.h"
 # include "../gnl/get_next_line.h"
 # include <stdio.h>
+typedef struct coord_s
+{
+	int x;
+	int y;
+	int z;
+}coord_t;
 void ft_hook(void* param);
 #endif
--- main.c
+++ main.c
@@ -6,49 +6,78 @@ static void error(void)
 	exit(EXIT_FAILURE);
 }
 
-void	put_pixel_on_map(mlx_image_t* img, mlx_t* mlx, char *path)
+/*Stockage des informations de la map dans une variable char*/
+char	**get_line(int fd)
 {
-	int	fd;
-	char	*read_line;
 	char	*buffer;
-	char	**tab;
-
-	fd = open(path, O_RDONLY);
+	char	*read_line;
+	char	**tab;	
 	read_line = ft_calloc(1, 1);
 	while (buffer)
 	{
 		buffer = ft_strdup(get_next_line(fd));
 		read_line = ft_strjoin(read_line, buffer);
 	}
-	printf("%s",read_line);
 	tab = ft_split(read_line, ' ');
-	int i = 0;
-	int y = 0;
-	int x = 0;
+	return (tab);
+}
+
+/*Ouverture du fichier .fdf*/
+int	open_file(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		exit(EXIT_FAILURE);
+	
+	return (fd);
+	
+}
+void	put_pixel_on_map(mlx_image_t* img, mlx_t* mlx, char *path)
+{
+	int	fd;
+	char	**tab;
+	coord_t *coord;
+	int	i;
+	
+	i = 0;
+	coord->x = 0;
+	coord->y = 0;
+	coord->z = 0;
+	/*Lecture de la map*/
+	fd = open_file(path);
+	/*Stockage des infos map dans ma variable read_line*/
+	tab = get_line(fd);
 	while (tab[i])
 	{
-		mlx_put_pixel(img, x, y, 0xFF000000);
+		/*Ici je divise img->height et width afin de placer ma map au centre de la page*/
+		mlx_put_pixel(img, coord->x + (img->width / 2), coord->y + (img->height / 2), 0xFF000000);
 		if (tab[i][1] != '\n')
 		{
-			x++;
+			coord->x++;
 		}
 		else
 		{
-			x = 0;
-			y++;
+			coord->x = 0;
+			coord->y++;
 		}
 		i++;
-		printf("ok");
 	}
-	printf("ok");
+	mlx_resize_image(img, WIDTH, HEIGHT);
+	while (*tab)
+	{
+		printf("%s",*tab);
+		*tab++;
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		mlx_set_setting(MLX_MAXIMIZED, false);
-		mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42FDF", false);
+		mlx_set_setting(MLX_MAXIMIZED, true);
+		mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42FDF", true);
 		if (!mlx)
 			error();
 		mlx_image_t* img = mlx_new_image(mlx, mlx->width, mlx->height);
