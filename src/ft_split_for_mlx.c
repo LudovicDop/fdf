6dcedd27ef172843d5fb2e328938f1e11bca9ac6
Ludovic Doppler <ldoppler@k2r3p5.42mulhouse.fr>
2023-12-15 17:42:24 +0100

    in progress

--- src/ft_split_for_mlx.c
+++ src/ft_split_for_mlx.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:03:56 by ldoppler          #+#    #+#             */
-/*   Updated: 2023/12/15 17:18:27 by ldoppler         ###   ########.fr       */
+/*   Updated: 2023/12/15 17:26:06 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,23 @@ static int how_many_split(char *string)
 
 static char **implement(char *string, char **ret)
 {
-    
+    int i;
+    int j;
+    int k;
+
+    j = 0;
+    k = 0;
+    i = 0;
+    while(string[i])
+    {
+        if (string[i] != ' ' && string[i] != '\n')
+        {
+            ret[j][k] = string[i];
+        }
+        if (string[i] == '\n')
+            j++;
+        i++;
+    }
 }
 
 static int  each_word_memory(char *string, char **ret)
