b67c1574bb2d68119dc96c552bc81b5a9d329dde
Ludovic Doppler <ldoppler@k2r3p2.42mulhouse.fr>
2023-12-19 15:57:35 +0100

    improve ft_split_for_mlx

--- src/drawing.c
+++ src/drawing.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 16:37:50 by ldoppler          #+#    #+#             */
-/*   Updated: 2023/12/17 17:40:12 by ldoppler         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:57:09 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,18 +68,21 @@ void	put_pixel_on_map(t_info* info, char *path)
 		tmp = get_next_line(fd);
 		tab = ft_strjoin(tab, tmp);
 	}
+	//info->tab2d = ft_split_for_mlx(tab);
 	info->tab2d = ft_split_for_mlx(tab);
+	printf("ret[%d][%d] = %s\n",0,2,info->tab2d[6][2]);
+	//printf("%s\n",info->tab2d[5][4]);
 	//printf("tab[%d][%d] = %p\n",increase.i, increase.j,info->tab2d[7]);
-	while (info->tab2d[increase.i] != NULL)
-	{
-		increase.j = 0;
-		while (info->tab2d[increase.i][increase.j] != NULL)
-		{
-				printf("tab[%d][%d] = %s\n",increase.i, increase.j,info->tab2d[increase.i][increase.j]);
-				increase.j++;
-		}
-		increase.i++;
-	}
+	// while (info->tab2d[increase.i] != NULL)
+	// {
+	// 	increase.j = 0;
+	// 	while (info->tab2d[increase.i][increase.j] != NULL)
+	// 	{
+	// 			printf("tab[%d][%d] = %s\n",increase.i, increase.j,info->tab2d[increase.i][increase.j]);
+	// 			increase.j++;
+	// 	}
+	// 	increase.i++;
+	// }
 	mlx_image_to_window(info->mlx, info->img, 250, 250);
 	info->refresh = 0;
 }
--- src/ft_split_for_mlx.c
+++ src/ft_split_for_mlx.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:03:56 by ldoppler          #+#    #+#             */
-/*   Updated: 2023/12/17 18:03:49 by ldoppler         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:53:30 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,42 +24,43 @@ static int first_dimension(char *string)
     {
         if (string[i] == ' ')
         {
-            //printf("ok\n");
             k++;
+            while(string[i] == ' ')
+                i++;
         }
         if (string[i] == '\n')
-        {  
-            k++;
-            //printf("malloc first dimension = %d\n",k);
+        {
+            printf("Malloc de x = %d\n",k);
             return (k);
         }
         i++;
     }
 }
-
-static int second_dimension(char *string,char ***ret)
+//ret[x] = (char**)ft_calloc(sizeof(char*), first_dimension(string) + 1);
+static int second_dimension(char *string,char ***ret,int first_dim)
 {
     int i;
-    int x;
     int y;
 
     y = 0;
-    x = 0;
     i = 0;
     while (string[i])
     {
-        if (string[i] == ' ' || string[i] == '\n')
-        {
-            //printf("ret[%d] = x\n",x);
-            ret[x] = (char**)ft_calloc(sizeof(char*), first_dimension(string) + 1);
-            x++;
-        }
         if (string[i] == '\n')
-            break;;
+            y++;
         i++;
     }
+    i = 0;
+    while (i < first_dim)
+    {
+        printf("ret[%d] = calloc(%d + 1)\n",i,y);
+        ret[i] = (char**)ft_calloc(sizeof(char*), y + 1);
+        i++;
+    }
+    printf("y value = %d\n",y);
+    return (y);
 }
-
+//ret[x][y] = (char*)ft_calloc(sizeof(char),each_word + 1);
 static int each_word(char *string, char ***ret)
 {
     int i;
@@ -73,32 +74,32 @@ static int each_word(char *string, char ***ret)
     i = 0;
     while (string[i])
     {
-        if (string[i] != ' ' && string[i] != '\n')
+        while (string[i] != ' ' && string[i] != '\n')
         {
             each_word++;
-            //printf("ret[%d][%d] = %c\n",x, y, string[i]);
+            i++;
         }
         if (string[i] == '\n')
         {
-            //printf("=>ret[%d][%d] = %c\n",x, y, string[i]);
-            //printf("ret[%d][%d] = %c\n",x, y, string[i]);
+            printf("ret[%d][%d] = %d\n",x,y,each_word + 1);
             ret[x][y] = (char*)ft_calloc(sizeof(char),each_word + 1);
-            //printf("each_word = %d\n",each_word);
             each_word = 0;
             x = 0;
             y++;
+            i++;
         }
         if (string[i] == ' ')
         {
-            //printf("ret[%d][%d] = %c\n",x, y, string[i]);
-            //printf("each_word = %d\n",each_word);
+            printf("|ret[%d][%d] = %d\n",x,y,each_word + 1);
             ret[x][y] = (char*)ft_calloc(sizeof(char),each_word + 1);
             each_word = 0;
             x++;
+            while (string[i] && string[i] == ' ')
+                i++;
         }
-        i++;
     }
 }
+
 static char*** implement(char *string, char ***ret)
 {
     int i;
@@ -106,47 +107,43 @@ static char*** implement(char *string, char ***ret)
     int y;
     int each_word;
 
+    each_word = 0;
     x = 0;
     y = 0;
     i = 0;
     while (string[i])
     {
-        if (string[i] != ' ' && string[i] != '\n')
+        while (string[i] != ' ' && string[i] != '\n')
         {
-            //printf("ret[%d][%d][%d] = %c\n",x, y,each_word, string[i]);
-            ret[x][y][each_word] = string[i];
+            ret[x][y][each_word] = string[i]; 
             each_word++;
+            i++;
         }
         if (string[i] == '\n')
         {
-            //printf("=>ret[%d][%d] = %c\n",x, y, string[i]);
-            //printf("ret[%d][%d] = %c\n",x, y, string[i]);
-            //printf("each_word = %d\n",each_word);
-            //printf("ret[%d][%d][%d] = %c\n",x, y,each_word, string[i]);
-            ret[x][y][each_word] = '\0';
             each_word = 0;
             x = 0;
             y++;
+            i++;
         }
         if (string[i] == ' ')
         {
-            //printf("ret[%d][%d] = %c\n",x, y, string[i]);
-            //printf("each_word = %d\n",each_word);
-            //printf("ret[%d][%d][%d] = %c\n",x, y,each_word, string[i]);
-            ret[x][y][each_word] = '\0';
             each_word = 0;
             x++;
+            while (string[i] && string[i] == ' ')
+                i++;
         }
-        i++;
     }
 }
 
 char    ***ft_split_for_mlx(char *string)
 {
     char    ***ret;
+    int     first_dim;
 
-    ret = (char***)ft_calloc(sizeof(char**),first_dimension(string) + 1);
-    second_dimension(string, ret);
+    first_dim = first_dimension(string) + 1;
+    ret = (char***)ft_calloc(sizeof(char**),first_dim);
+    second_dimension(string, ret, first_dim);
     each_word(string,ret);
     implement(string,ret);
     return (ret);
